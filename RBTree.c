3dc3f9becf386ecc3464b315e98a4eab96cee828
Makuk369 <makuk369@gmail.com>
2025-03-22 12:45:42 +0100

    rb basic input

--- RBTree.c
+++ RBTree.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RED 1
+#define BLACK 0
+
 typedef struct node {
     int value;
     int color; // 1-red, 0-black
@@ -10,12 +13,33 @@ typedef struct node {
 } Node;
 
 Node* createNode(int value);
+Node* insertNode(Node* root, int value);
 
 void indent(int tabCount);
 void printTree(Node* root, int level);
 
 int main()
 {
+    Node* root = createNode(5);
+
+    root = insertNode(root, 3);
+    root = insertNode(root, 8);
+    root = insertNode(root, 4);
+    root = insertNode(root, 1);
+    // root = insertNode(root, 10);
+    // root = insertNode(root, 6);
+
+    printTree(root, 0);
+    printf("\n");
+
+    // root = deleteNode(root, 5);
+
+    // printTree(root, 0);
+
+    // int numToFind = 9;
+    // printf("found %d = %s\n", numToFind, findNode(root, numToFind) ? "true" : "false");
+
+    free(root);
     return 0;
 }
 
@@ -26,8 +50,36 @@ Node* createNode(int value){
         result->left = NULL;
         result->right = NULL;
         result->value = value;
+        result->color = RED;
     }
     return result;
+}
+
+Node* insertNode(Node* root, int value){
+    // add node
+    if(root == NULL){
+        root = createNode(value);
+        return root;
+    }
+
+    // duplicate value == invalid
+    if(value == root->value){
+        printf("!!! error: No duplicate values allowed !!!\n");
+        return root;
+    }
+
+    // go deeper
+    if(value < root->value){
+        root->left = insertNode((root->left), value);
+        root->left->parent = root;
+    }
+    else{
+        root->right = insertNode((root->right), value);
+        root->right->parent = root;
+    }
+
+    // return without a change
+    return root;
 }
 
 void indent(int tabCount){
